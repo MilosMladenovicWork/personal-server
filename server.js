9b0339ae2188197c5f43c329184dd3348083cc5d
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-02-20 16:53:08 +0100

    made barebones email sender

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+
+
+const app = express()
+
+
+const port = process.env.PORT
+
+app.use(express.json())
+
+app.post('/contact', async (req, res) => {
+  try{
+    const msg = {
+      to: 'milos.mladenovic.work@gmail.com',
+      from: req.body.email,
+      subject: 'From ' + req.body.name,
+      text:req.body.message
+    }
+    await sgMail.send(msg);
+    res.status(200).json({message:'Hi there, ' + req.body.name + '. I am going to respond to your message as soon as I can. Regards, Milos Mladenovic.'})  
+  }catch(e){
+    res.status(500).json({error:'There was an error sending your mail'})
+  }
+})
+
+app.listen(port, () => {
+  console.log('Listening on port ' + port)
+})
