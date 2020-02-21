761fc9ef4bd2c33656ceeff1e442a023c97c9a43
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-02-21 17:37:33 +0100

    fixed server

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 
-
 const app = express()
 
 
 const port = process.env.PORT
 
 app.use(express.json())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://server-personal-use.herokuapp.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.post('/contact', async (req, res) => {
   try{
+    console.log(req.body)
     const msg = {
       to: 'milos.mladenovic.work@gmail.com',
       from: req.body.email,
@@ -22,7 +27,7 @@ app.post('/contact', async (req, res) => {
     await sgMail.send(msg);
     res.status(200).json({message:'Hi there, ' + req.body.name + '. I am going to respond to your message as soon as I can. Regards, Milos Mladenovic.'})  
   }catch(e){
-    res.status(500).json({error:'There was an error sending your mail'})
+    res.status(500).json({"error":'There was an error sending your mail'})
   }
 })
 
